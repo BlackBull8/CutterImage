8bd8e0d780605fcec6abaf830d50598d29df58c6
helius <1741205686@qq.com>
2016-05-09 16:34:04 +0800

    热键功能的完成

--- Cutter_UI/HotKey.cs
+++ Cutter_UI/HotKey.cs
@@ -7,107 +7,23 @@ using System.Windows.Interop;
 namespace Cutter_UI
 {
     /// <summary>
-    /// 直接创建类实例就可以实现注册
-    /// 自动会完成注销
-    /// 注意注册时会抛出异常
+    /// 热键
     /// </summary>
     public class HotKey
     {
-        //热键编号
-        private int _keyId;
-        //窗体句柄
-        private IntPtr _handle;
-        //热键所在窗体
-        private Window _window;
-        //热键控制键
-        private uint _controlKey;
-        //热键主键
-        private uint _key;
-        //热键事件委托
-        public delegate void OnHotKeyEventHandler();
-        //热键事件
-        public event OnHotKeyEventHandler OnHotKey = null;
-        //热键哈希表
-        static Hashtable _keyPair=new Hashtable();
-        //热键信息编号
-        private const int WM_HOTKTY = 0x0312;
-        //控制键编码
-        public enum KeyFlags
-        {
-            MOD_ALT=0x1,
-            MOD_CONTROL=0x2,
-            MOD_SHIFT=0x4,
-            MOD_WIN=0x8
-        }
+        [System.Runtime.InteropServices.DllImport("user32.dll",SetLastError = true)]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint controlKey, Keys virtualKey);
 
-        /// <summary>
-        /// 构造函数，注册热键
-        /// </summary>
-        /// <param name="win">注册窗体</param>
-        /// <param name="control">控制键</param>
-        /// <param name="key">主键</param>
-        public HotKey(Window win, HotKey.KeyFlags control, Keys key)
-        {
-            _handle = new WindowInteropHelper(win).Handle;
-            _window = win;
-            _controlKey = (uint) control;
-            _key = (uint) key;
-            _keyId = (int) _controlKey + (int) _key*10;
-            if (HotKey._keyPair.ContainsKey(_keyId))
-            {
-                throw new Exception("热键已经被注册！");
-            }
-            //注册热键
-            if (HotKey.RegisterHotKey(_handle, _keyId, _controlKey, _key)==false)
-            {
-                //消息挂钩只能连接一次
-                throw new Exception("热键注册失败！");
-            }
-            if (HotKey._keyPair.Count == 0)
-            {
-                if (InstallHotKeyHook(this) == false)
-                {
-                    throw new Exception("消息挂钩连接失败！");
-                }
-            }
-            HotKey._keyPair.Add(_keyId,this);
-        }
+        [System.Runtime.InteropServices.DllImport("user32.dll",SetLastError = true)]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+    }
 
-        [System.Runtime.InteropServices.DllImport("user32")]
-        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint controlKey, uint virtualKey);
-
-        [System.Runtime.InteropServices.DllImport("usr32")]
-        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-
-        private static bool InstallHotKeyHook(HotKey hk)
-        {
-            if (hk._window == null || hk._handle == IntPtr.Zero)
-            {
-                return false;
-            }
-            //获得消息源
-            System.Windows.Interop.HwndSource source = System.Windows.Interop.HwndSource.FromHwnd(hk._handle);
-            if (source == null) return false;
-
-            //挂接事件
-            source.AddHook(HotKey.HotKeyHook);
-            return true;
-        }
-
-        //热键处理过程
-        private static IntPtr HotKeyHook(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
-        {
-            if (msg == WM_HOTKTY)
-            {
-                HotKey hk = (HotKey) HotKey._keyPair[(int) wparam];
-                if (hk.OnHotKey != null) hk.OnHotKey();
-            }
-            return IntPtr.Zero;
-        }
-
-        ~HotKey()
-        {
-            HotKey.UnregisterHotKey(_handle, _keyId);
-        }
+    public enum KeyFlags
+    {
+        MOD_NONE=0,
+        MOD_ALT = 1,
+        MOD_CTRL = 2,
+        MOD_SHIFT = 4,
+        MOD_WIN = 8
     }
 }
--- Cutter_UI/MainWindow.xaml.cs
+++ Cutter_UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -8,12 +9,15 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CutterLogical;
+using MessageBox = System.Windows.MessageBox;
 
 namespace Cutter_UI
 {
@@ -23,6 +27,7 @@ namespace Cutter_UI
     public partial class MainWindow : Window
     {
         private readonly MainCaptureScreen _mainCaptureScreen = new MainCaptureScreen();
+        private WindowInteropHelper _wndHelper;
         public MainWindow()
         {
             InitializeComponent();
@@ -45,12 +50,45 @@ namespace Cutter_UI
         {
             //指定事件
             _mainCaptureScreen.NotifyEventHanlder += _mainCaptureScreen_NotifyEventHanlder;
+            uint ctrlHotKey = (uint) (KeyFlags.MOD_ALT | KeyFlags.MOD_CTRL);
+            _wndHelper = new WindowInteropHelper(this);
+            if (!HotKey.RegisterHotKey(_wndHelper.Handle, 100, ctrlHotKey, Keys.A))
+            {
+                MessageBox.Show("热键已被注册，已不能使用！！请点击按钮进行操作");
+            }
         }
 
         private void _mainCaptureScreen_NotifyEventHanlder(object sender, EventArgs e)
         {
             //设置窗口状态
             WindowState=WindowState.Normal;
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            HotKey.UnregisterHotKey(_wndHelper.Handle, 100);
+        }
+
+        private void GlobalKeyProcess()
+        {
+            WindowState = WindowState.Minimized;
+            Thread.Sleep(300);
+            _mainCaptureScreen.StartToCapture();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {
+            if (wparam.ToInt32() == 100)
+            {
+                GlobalKeyProcess();
+            }
+            return IntPtr.Zero;
         }
     }
 }
